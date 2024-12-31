52c263be9d81fbc2682b09ce27f71d9418314ae2
adulee <hadidabdullah50@gmail.com>
2025-01-01 03:18:16 +0700

    mengisi readme sesauai kebutuhan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
         \App\Models\Tag::factory(50)->create();
         \App\Models\Post::factory(1500)->create();
 
-        // post_tag table has no Model so to seed it we need our own logic
+        
         for ($i = 0; $i < 1500; $i++) {
             DB::table('post_tag')->insertOrIgnore([
                 'post_id' => mt_rand(1, 1500),
